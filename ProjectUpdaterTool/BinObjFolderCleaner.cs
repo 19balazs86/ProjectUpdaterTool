25872e13c37b5ff5a1b9da57c5e314554ee79376
Balazs Kereszturi <19balazs86@gmail.com>
2022-10-23 16:25:52 +0200

    Add feature to clean bin and obj folders

---
+++ ProjectUpdaterTool/BinObjFolderCleaner.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectUpdaterTool
+{
+    public static class BinObjFolderCleaner
+    {
+        public static void CleanBinObjFolders()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            string[] binFolders = Directory.GetDirectories(currentDirectory, "bin", SearchOption.AllDirectories);
+
+            Console.WriteLine($"Deleting {binFolders.Length} of 'bin' folders.");
+
+            foreach (string folderPath in binFolders)
+                Directory.Delete(folderPath, true);
+
+            string[] objFolders = Directory.GetDirectories(currentDirectory, "obj", SearchOption.AllDirectories);
+
+            Console.WriteLine($"Deleting {binFolders.Length} of 'obj' folders.");
+
+            foreach (string folderPath in objFolders)
+                Directory.Delete(folderPath, true);
+        }
+    }
+}
---
+++ ProjectUpdaterTool/PackageUpdater.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace ProjectUpdaterTool
+{
+    public static class PackageUpdater
+    {
+        private static readonly Regex _dotnetOutdatedRegex = new Regex(@"([^\s]+)\s+([^\s]+)\s+->\s+([^\s]+)");
+
+        public static async Task Update(bool isTestMode)
+        {
+            try
+            {
+                string currentDirectory = Directory.GetCurrentDirectory();
+
+                IEnumerable<Package> packagesToUpdate = await getPackages(currentDirectory);
+
+                string[] csprojFiles = Directory.GetFiles(currentDirectory, "*.csproj", SearchOption.AllDirectories);
+
+                List<Result> results = new List<Result>();
+
+                foreach (string csprojFilePath in csprojFiles)
+                {
+                    string csprojContent = File.ReadAllText(csprojFilePath);
+
+                    Result result = null;
+
+                    foreach (Package package in packagesToUpdate)
+                    {
+                        Regex regex = new Regex($"Include=\"{package.PackageName}\" Version=\"{package.VersionOld}\"");
+
+                        if (regex.IsMatch(csprojContent))
+                        {
+                            if (result is null)
+                                result = new Result(csprojFilePath.Replace(currentDirectory, string.Empty));
+
+                            result.Packages.Add(package.PackageName);
+
+                            string withNewVersion = $"Include=\"{package.PackageName}\" Version=\"{package.VersionNew}\"";
+
+                            if (!isTestMode)
+                                csprojContent = regex.Replace(csprojContent, withNewVersion);
+                        }
+                    }
+
+                    if (result is null) continue;
+
+                    results.Add(result);
+
+                    if (!isTestMode)
+                        await saveCsprojContent(csprojContent, csprojFilePath);
+                }
+
+                using Stream resultsOutStream = File.Create(Path.Combine(currentDirectory, "PackagesToUpdateResult.json"));
+
+                await JsonSerializer.SerializeAsync(resultsOutStream, results, new JsonSerializerOptions { WriteIndented = true });
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+            }
+        }
+
+        private static async Task<IEnumerable<Package>> getPackages(string currentDirectory)
+        {
+            List<Package> packages = new List<Package>();
+
+            string input = await File.ReadAllTextAsync(Path.Combine(currentDirectory, "PackagesToUpdate.txt"));
+
+            foreach (Match match in _dotnetOutdatedRegex.Matches(input))
+            {
+                var package = new Package
+                {
+                    PackageName = match.Groups[1].Value,
+                    VersionOld  = match.Groups[2].Value,
+                    VersionNew  = match.Groups[3].Value
+                };
+
+                packages.Add(package);
+            }
+
+            return packages;
+        }
+
+        private static async Task saveCsprojContent(string csprojContent, string csprojFilePath)
+        {
+            using FileStream csprojOutStream = File.Create(csprojFilePath);
+
+            // BOM
+            csprojOutStream.WriteByte(0xEF);
+            csprojOutStream.WriteByte(0xBB);
+            csprojOutStream.WriteByte(0xBF);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(csprojContent);
+
+            await csprojOutStream.WriteAsync(bytes, 0, bytes.Length);
+            await csprojOutStream.FlushAsync();
+        }
+    }
+}
